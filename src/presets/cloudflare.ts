a1514e6340502b67d3522653d2f2b2524d120eeb
Tim Hanlon <tim@timhanlon.com>
2022-11-19 11:07:02 +1100

    add cloudflare-esm preset

--- src/presets/cloudflare.ts
+++ src/presets/cloudflare.ts
@@ -23,3 +23,24 @@ export const cloudflare = defineNitroPreset({
     },
   },
 });
+
+export const cloudflareEsm = defineNitroPreset({
+  extends: 'base-worker',
+  entry: '#internal/nitro/entries/cloudflare-esm',
+  commands: {
+    preview: 'npx wrangler dev ./server/index.mjs --site ./public --local',
+    deploy: 'npx wrangler publish'
+  },
+  rollupConfig: {
+    external: '__STATIC_CONTENT_MANIFEST',
+    output: {
+      format: 'esm'
+    }
+  },
+  hooks: {
+    async 'compiled' (nitro: Nitro) {
+      await writeFile(resolve(nitro.options.output.dir, 'package.json'), JSON.stringify({ private: true, main: './server/index.mjs' }, null, 2))
+      await writeFile(resolve(nitro.options.output.dir, 'package-lock.json'), JSON.stringify({ lockfileVersion: 1 }, null, 2))
+    }
+  }
+});
