a7f8cfcc7e5be7ab90ff11220a4b9efaff19c20a
Tim Hanlon <tim@timhanlon.com>
2023-03-04 11:49:35 +1100

    chore: lint

--- src/presets/cloudflare.ts
+++ src/presets/cloudflare.ts
@@ -25,23 +25,29 @@ export const cloudflare = defineNitroPreset({
 });
 
 export const cloudflareEsm = defineNitroPreset({
-  extends: 'base-worker',
-  entry: '#internal/nitro/entries/cloudflare-esm',
+  extends: "base-worker",
+  entry: "#internal/nitro/entries/cloudflare-esm",
   commands: {
-    preview: 'npx wrangler dev ./server/index.mjs --site ./public --local',
-    deploy: 'npx wrangler publish'
+    preview: "npx wrangler dev ./server/index.mjs --site ./public --local",
+    deploy: "npx wrangler publish",
   },
   rollupConfig: {
-    external: '__STATIC_CONTENT_MANIFEST',
+    external: "__STATIC_CONTENT_MANIFEST",
     output: {
-      format: 'esm',
-      exports: 'named'
-    }
+      format: "esm",
+      exports: "named",
+    },
   },
   hooks: {
-    async 'compiled' (nitro: Nitro) {
-      await writeFile(resolve(nitro.options.output.dir, 'package.json'), JSON.stringify({ private: true, main: './server/index.mjs' }, null, 2))
-      await writeFile(resolve(nitro.options.output.dir, 'package-lock.json'), JSON.stringify({ lockfileVersion: 1 }, null, 2))
-    }
-  }
+    async compiled(nitro: Nitro) {
+      await writeFile(
+        resolve(nitro.options.output.dir, "package.json"),
+        JSON.stringify({ private: true, main: "./server/index.mjs" }, null, 2)
+      );
+      await writeFile(
+        resolve(nitro.options.output.dir, "package-lock.json"),
+        JSON.stringify({ lockfileVersion: 1 }, null, 2)
+      );
+    },
+  },
 });
